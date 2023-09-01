88d6b46ad7bc87e537854fc7a317da5756e88c4b
Kaivan Kamali <kxk302@psu.edu>
2023-08-31 22:29:31 -0400

    REvised script so you can specify the extension of fasta files

--- scripts/run_quadron_for_hg19.sh
+++ scripts/run_quadron_for_hg19.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
+if [ $# -lt 2 ]; then
   echo "Incorrect number of parameters"
-  echo "Specify the input fasta files folder and the output folder"
+  echo "Specify the input fasta files folder, the output folder, and optionally, the fasta file extension (default: .fa)"
   exit
 fi
 
 FastaFilesFolder=$1
 OutputFolder=$2
+FastaExt=${3:-".fa"}
 
 echo "FastaFilesFolder: <$FastaFilesFolder>"
 echo "OutputFolder: <$OutputFolder>"
+echo "FastaExt: <$FastaExt>"
 
-for file_path in $(ls ${FastaFilesFolder}/*.fa)
+for file_path in $(ls ${FastaFilesFolder}/*${FastaExt})
 do
   file_name=$(basename ${file_path})
-  ./scripts/run_quadron.sh  ${file_path} ${OutputFolder}/${file_name/.fa/_out.txt} 8
+  ./scripts/run_quadron.sh  ${file_path} ${OutputFolder}/${file_name/${FastaExt}/_out.txt} 8
 done
