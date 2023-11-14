51e31d20d2f8a0238cfc2c3db1af5341f2d758b2
Saswat KM <116586251+saswat-km@users.noreply.github.com>
2023-11-14 18:25:23 -0500

    Update quadron_txt2bed.py
    The end coordinate in .BED format is not included in the segment. The segment looks like [start,end).

--- scripts/quadron_txt2bed.py
+++ scripts/quadron_txt2bed.py
@@ -12,7 +12,7 @@ def textToBed(input_text_file, output_bed_file, chromosome_number):
       if crit[0] == "DATA:" and crit[4] != "NA":
           start = int(crit[1])
           length = int(crit[3])
-          f.write("chr%s\t%i\t%i\t%.2f\t%s\t%i\n" %(chromosome_number, start-adjust, length+start-adjust-1, float(crit[4]), crit[2], length))
+          f.write("chr%s\t%i\t%i\t%.2f\t%s\t%i\n" %(chromosome_number, start-adjust, length+start-adjust, float(crit[4]), crit[2], length))
 
 
 if __name__ == "__main__":
