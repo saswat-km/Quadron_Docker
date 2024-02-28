c5a0447a35bd674e56c727db9095e20a8917a195
Kaivan Kamali <kxk302@psu.edu>
2024-02-28 11:38:14 -0500

    Revised script that computes the overlap between G4Hunter and Quadron, so the sum of the intersect intervals is used. Moved requirements.txt file to the top level directory

--- scripts/quadron_vs_g4hunter.py
+++ scripts/quadron_vs_g4hunter.py
@@ -6,90 +6,56 @@ from os import path
 import pandas as pd
 from matplotlib_venn import venn2
 
-column_names = ['chr1','start1','end1','score1','strand1','length1','chr2','start2','end2','score2','strand2','length2']
+column_names = ['chr', 'start', 'end', 'score', 'length', 'strand']
 
-def quadron_vs_g4hunter(quadron_dir, g4hunter_dir, chromosome_num, overlap_percentage, 
-                        g4hunter_window_size, g4hunter_threshold, output_dir, species):
+def quadron_vs_g4hunter(quadron_dir, g4hunter_dir, chromosome_num, g4hunter_window_size,
+                        g4hunter_threshold, output_dir, species):
 
-  quadron_bed_file_name = f"quadron_{species}_chr{chromosome_num}.bed"
+  quadron_bed_file_name = f"chr{chromosome_num}_out.bed"
   quadron_bed_file_path = path.join(quadron_dir, quadron_bed_file_name)
   print(f'quadron_bed_file_path: {quadron_bed_file_path}')
+  df_quadron = pd.read_csv(quadron_bed_file_path, sep='\t', names=column_names, header=None)
+  quadron_intervals_length = df_quadron["length"].sum()
+  print(f"quadron_intervals_length: {quadron_intervals_length}")
 
-  g4hunter_bed_file_name = f"g4hunter_{species}_{g4hunter_window_size}_{chromosome_num}_{g4hunter_threshold}.bed"
+  g4hunter_bed_file_name = f"chr{chromosome_num}-W{g4hunter_window_size}-S{g4hunter_threshold}-Merged-pruned.bed"
   g4hunter_bed_file_path = path.join(g4hunter_dir, g4hunter_bed_file_name)
   print(f'g4hunter_bed_file_path: {g4hunter_bed_file_path}')
+  df_g4hunter = pd.read_csv(g4hunter_bed_file_path, sep='\t', names=column_names, header=None)
+  g4hunter_intervals_length = df_g4hunter["length"].sum()
+  print(f"g4hunter_intervals_length: {g4hunter_intervals_length}")
 
-  quadron_output_file_name = f"{species}_chr{chromosome_num}_window{g4hunter_window_size}_threshold{g4hunter_threshold}_quadron.bed"
-  quadron_output_file_path = path.join(output_dir, quadron_output_file_name)
-  print(f'quadron_output_file_path: {quadron_output_file_path}')
+  intersect_file_name = f"{species}_chr{chromosome_num}_window{g4hunter_window_size}_threshold{g4hunter_threshold}_intersect.bed"
+  intersect_file_path = path.join(output_dir, intersect_file_name)
+  print(f'intersect_file_path: {intersect_file_path}')
 
-  g4hunter_output_file_name =f"{species}_chr{chromosome_num}_window{g4hunter_window_size}_threshold{g4hunter_threshold}_g4hunter.bed"
-  g4hunter_output_file_path = path.join(output_dir, g4hunter_output_file_name)
-  print(f'g4hunter_output_file_path: {g4hunter_output_file_path}')
+  bedtools_intersect_cmd = f'bedtools intersect -a {quadron_bed_file_path} -b {g4hunter_bed_file_path}'
+  process = subprocess.Popen(bedtools_intersect_cmd.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+  print(f'error: {error}')
+
+  with open(intersect_file_path, "w") as fp:
+    fp.write(output.decode())
+
+  df_intersect = pd.read_csv(intersect_file_path, sep='\t', names=["chr", "start", "end"], header=None, usecols=range(3))
+  df_intersect["length"] = df_intersect["end"].astype(int) - df_intersect["start"].astype(int)
+  intersect_intervals_length = df_intersect["length"].sum()
+  print(f"intersect_intervals_length: {intersect_intervals_length}")
 
   venn_diagram_output_file_name =f"{species}_chr{chromosome_num}_window{g4hunter_window_size}_threshold{g4hunter_threshold}_venn.png"
   venn_diagram_output_file_path = path.join(output_dir, venn_diagram_output_file_name)
   print(f'venn_diagram_output_file_path: {venn_diagram_output_file_path}')
 
-  generate_quadron_report(quadron_bed_file_path, g4hunter_bed_file_path, chromosome_num, overlap_percentage,
-                          g4hunter_window_size, g4hunter_threshold, output_dir, species, quadron_output_file_path)
-
-  generate_g4hunter_report(quadron_bed_file_path, g4hunter_bed_file_path, chromosome_num, overlap_percentage,
-                           g4hunter_window_size, g4hunter_threshold, output_dir, species, g4hunter_output_file_path)
-
-  generate_venn_diagram(quadron_output_file_path, g4hunter_output_file_path, venn_diagram_output_file_path)
+  generate_venn_diagram(quadron_intervals_length, g4hunter_intervals_length, intersect_intervals_length, venn_diagram_output_file_path)
 
 
-def generate_quadron_report(quadron_bed_file_path, g4hunter_bed_file_path, chromosome_num, overlap_percentage,
-                            g4hunter_window_size, g4hunter_threshold, output_dir, species, output_file_path):
-
-  bedtools_intersect_cmd = f'bedtools intersect -wa -wb -f {overlap_percentage} -r -a {quadron_bed_file_path} -b {g4hunter_bed_file_path} -loj'
-
-  process = subprocess.Popen(bedtools_intersect_cmd.split(), stdout=subprocess.PIPE)
-  output, error = process.communicate()
-  # print(f'output: {output}')
-  print(f'error: {error}')
-  
-  with open(output_file_path, "w") as fp:
-    fp.write(output.decode())
-
-def generate_g4hunter_report(quadron_bed_file_path, g4hunter_bed_file_path, chromosome_num, overlap_percentage,
-                             g4hunter_window_size, g4hunter_threshold, output_dir, species, output_file_path):
-
-  bedtools_intersect_cmd = f'bedtools intersect -wa -wb -f {overlap_percentage} -r -a {g4hunter_bed_file_path} -b {quadron_bed_file_path} -loj'
-
-  process = subprocess.Popen(bedtools_intersect_cmd.split(), stdout=subprocess.PIPE)
-  output, error = process.communicate()
-  # print(f'output: {output}')
-  print(f'error: {error}')
-  
-  with open(output_file_path, "w") as fp:
-    fp.write(output.decode())
-
-
-def generate_venn_diagram(quadron_output_file_path, g4hunter_output_file_path, venn_diagram_output_file_path):
-  df_quadron = pd.read_csv(quadron_output_file_path, sep='\t', names=column_names, header=None)
-  quadron_intersect = df_quadron[df_quadron['chr2'] != '.'].shape[0]
-  quadron_only = df_quadron[df_quadron['chr2'] == '.'].shape[0]
-
-  df_g4hunter = pd.read_csv(g4hunter_output_file_path, sep='\t', names=column_names, header=None)
-  g4hunter_intersect = df_g4hunter[df_g4hunter['chr2'] != '.'].shape[0]
-  g4hunter_only = df_g4hunter[df_g4hunter['chr2'] == '.'].shape[0]
-  quadron_intersect_percentage = quadron_intersect / (quadron_intersect + quadron_only)
-
+def generate_venn_diagram(quadron_intervals_length, g4hunter_intervals_length, intersect_intervals_length, venn_diagram_output_file_path):
   venn_diagram_output_file_name = path.basename(venn_diagram_output_file_path)
   venn_diagram_title = venn_diagram_output_file_name.replace('_venn.png', '')
 
-  print(f'quadron_output_file_path: {quadron_output_file_path}')
-  print(f'quadron_intersect: {quadron_intersect}')
-  print(f'quadron_only: {quadron_only}')
-  print(f'g4hunter_output_file_path: {g4hunter_output_file_path}')
-  print(f'g4hunter_intersect: {g4hunter_intersect}')
-  print(f'g4hunter_only: {g4hunter_only}')
-
   plt.figure(figsize=(4,4))
   plt.title(venn_diagram_title)
-  v = venn2(subsets = (quadron_only, g4hunter_only, quadron_intersect), set_labels = ('Quadron G4s', 'G4Hunter G4s'))
+  v = venn2(subsets = (quadron_intervals_length, g4hunter_intervals_length, intersect_intervals_length), set_labels = ('Quadron G4s', 'G4Hunter G4s'))
   plt.savefig(venn_diagram_output_file_path)
 
 
@@ -99,14 +65,10 @@ if __name__ == "__main__":
   argumentParser.add_argument("-q", "--quadron_dir", type=str, required=True)
   argumentParser.add_argument("-g", "--g4hunter_dir", type=str, required=True)
   argumentParser.add_argument("-n", "--chromosome_num", type=str, required=True)
-  argumentParser.add_argument("-p", "--overlap_percentage", type=float, required=True)
   argumentParser.add_argument("-w", "--g4hunter_window_size", type=int, required=True)
   argumentParser.add_argument("-t", "--g4hunter_threshold", type=float, required=True)
   argumentParser.add_argument("-o", "--output_dir", type=str, required=True)
-  argumentParser.add_argument("-s", "--species", type=str, required=True, default="hsapiens", choices=["hsapiens"])
+  argumentParser.add_argument("-s", "--species", type=str, required=True)
 
   args = argumentParser.parse_args()
-  quadron_vs_g4hunter(args.quadron_dir, args.g4hunter_dir, args.chromosome_num, args.overlap_percentage, args.g4hunter_window_size, args.g4hunter_threshold, args.output_dir, args.species)
-
-
-
+  quadron_vs_g4hunter(args.quadron_dir, args.g4hunter_dir, args.chromosome_num, args.g4hunter_window_size, args.g4hunter_threshold, args.output_dir, args.species)
--- scripts/quadron_vs_g4hunter.sh
+++ scripts/quadron_vs_g4hunter.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 7 ]; then 
+if [ $# -ne 5 ]; then
     echo "Incorrect number of parameters"
-    echo "Specify Quadron bed file directory, G4Hunter bed file directory, bedtools intersect overlap percentage, G4Hunter window size, g4Hunter threshold, output directory, and species"
+    echo "Specify Quadron bed file directory, G4Hunter bed file directory, G4Hunter window size, G4Hunter threshold, and output directory"
     exit
 fi
 
 quadron_dir=$1
 g4hunter_dir=$2
-overlap_percentage=$3
-g4hunter_window_size=$4
-g4hunter_threshold=$5
-output_dir=$6
-species=$7
+g4hunter_window_size=$3
+g4hunter_threshold=$4
+output_dir=$5
 
 echo "quadron_dir: <$quadron_dir>"
 echo "g4hunter_dir: <$g4hunter_dir>"
-echo "overlap_percentage: <$overlap_percentage>"
 echo "g4hunter_window_size: <$g4hunter_window_size>"
 echo "g4hunter_threshold: <$g4hunter_threshold>"
 echo "output_dir: <$output_dir>"
-echo "species: <$species>"
 
-for i in {1..22}; do python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir -g $g4hunter_dir -n $i -p $overlap_percentage -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir -s $species; done
+for species in "Gorilla_gorilla" "Pan_troglodytes" "Symphalangus_syndactylus" "Homo_sapiens" "Pongo_abelii" "Pan_paniscus" "Pongo_pygmaeus"
+do
+  echo "species: ${species}"
 
-python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir -g $g4hunter_dir -n X -p $overlap_percentage -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir -s $species
+  for i in {1..22}; do python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir/$species -g $g4hunter_dir/$species -n $i -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir/$species -s $species; done
 
-python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir -g $g4hunter_dir -n Y -p $overlap_percentage -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir -s $species
+  python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir/$species -g $g4hunter_dir/$species -n X -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir/$species -s $species;
 
+  python3 ./scripts/quadron_vs_g4hunter.py -q $quadron_dir/$species -g $g4hunter_dir/$species -n Y -w $g4hunter_window_size -t $g4hunter_threshold -o $output_dir/$species -s $species;
+done
